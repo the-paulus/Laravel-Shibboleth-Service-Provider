07d60ef785042998d782a2451ac2bdf044b677c5
Paul Lyon <pmlyon@gmail.com>
2018-06-08 15:24:34 -0500

    Fixed syntax error.

--- src/database/migrations/2014_02_11_192529_create_users_table.php
+++ src/database/migrations/2014_02_11_192529_create_users_table.php
@@ -13,63 +13,64 @@ class CreateUsersTable extends Migration
     public function up()
     {
 
-      if(!Schema::hasTable('users')) {
+        if (!Schema::hasTable('users')) {
 
-        Schema::create('users', function ($table) {
-          $table->increments('id');
+            Schema::create('users', function ($table) {
+                $table->increments('id');
 
-          $table->string('email')->unique();
-          $table->string('first_name');
-          $table->string('last_name');
+                $table->string('email')->unique();
+                $table->string('first_name');
+                $table->string('last_name');
 
-          $table->enum('type', array('shibboleth', 'local'))->default('shibboleth');
+                $table->enum('auth_type', array('shibboleth', 'local'))->default('shibboleth');
 
-          $table->string('password', 60)->nullable()->default(null);
+                $table->string('password', 60)->nullable()->default(null);
 
-          $table->timestamps();
-          $table->softDeletes();
-        });
+                $table->timestamps();
+                $table->softDeletes();
+            });
 
-      } else {
+        } else {
 
-        Schema::table('users', function(BluePrint $table) {
+            Schema::table('users', function (BluePrint $table) {
 
-          $columns = $table->getColumns();
+                $columns = $table->getColumns();
 
-          if(is_null($columns->get('email'))) {
-            $table->string('email')->unique();
-          }
+                if (is_null($columns->get('email'))) {
+                    $table->string('email')->unique();
+                }
 
-          if(is_null($columns->get('first_name'))) {
-            $table->string('first_name');
-          }
+                if (is_null($columns->get('first_name'))) {
+                    $table->string('first_name');
+                }
 
-          if(is_null($columns->get('last_name'))) {
-            $table->string('last_name');
-          }
+                if (is_null($columns->get('last_name'))) {
+                    $table->string('last_name');
+                }
 
-          if(is_null($columns->get('auth_type'))) {
-            $table->enum('auth_type', array('shibboleth', 'local'))->default('shibboleth');
-          }
+                if (is_null($columns->get('auth_type'))) {
+                    $table->enum('auth_type', array('shibboleth', 'local'))->default('shibboleth');
+                }
 
-          if(is_null($columns->get('password'))) {
-            $table->string('password', 60)->nullable()->default(null);
-          }
+                if (is_null($columns->get('password'))) {
+                    $table->string('password', 60)->nullable()->default(null);
+                }
 
-          if(is_null($columns->get('created_at'))) {
-            $table-> timestamp('created_at', 0)->nullable();
-          }
+                if (is_null($columns->get('created_at'))) {
+                    $table->timestamp('created_at', 0)->nullable();
+                }
 
-          if(is_null($columns->get('upated_at'))) {
-            $table-> timestamp('upated_at', 0)->nullable();
-          }
+                if (is_null($columns->get('upated_at'))) {
+                    $table->timestamp('upated_at', 0)->nullable();
+                }
 
-          if(is_null($columns->get('deleted_at'))) {
-            $table-> timestamp('deleted_at', 0)->nullable();
-          }
+                if (is_null($columns->get('deleted_at'))) {
+                    $table->timestamp('deleted_at', 0)->nullable();
+                }
 
-      });
+            });
 
+        }
     }
 
     /**
