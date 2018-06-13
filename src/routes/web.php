fbdcb7091060e887c31d8d013416af2c34a58c7f
Paul Lyon <pmlyon@gmail.com>
2018-06-13 16:09:52 -0500

    Updated web routes.

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,23 +1,25 @@
 <?php
+use ThePaulus\Shibboleth\Controllers\ShibbolethController;
+
 Route::group(['middleware' => 'web'], function() {
   // Login Route (Shibboleth)
-  Route::get('/login', 'ThePaulus\Shibboleth\Controllers\ShibbolethController@create');
+  Route::get('login', ShibbolethController::class . '@create');
   // Logout Route (Shibboleth and Local)
-  Route::get('/logout', 'ThePaulus\Shibboleth\Controllers\ShibbolethController@destroy');
+  Route::get('logout', ShibbolethController::class . '@destroy');
   // Shibboleth IdP Callback
-  Route::get('/idp', 'ThePaulus\Shibboleth\Controllers\ShibbolethController@idpAuthorize');
+  Route::get('idp', ShibbolethController::class . '@idpAuthorize');
 
   // Login Route (Local)
-  Route::get('/local', 'ThePaulus\Shibboleth\Controllers\ShibbolethController@localCreate');
+  Route::get('local', ShibbolethController::class . '@localCreate');
   // Login Callback (Local)
-  Route::post('/local', 'ThePaulus\Shibboleth\Controllers\ShibbolethController@localAuthorize');
+  Route::post('local', ShibbolethController::class . '@localAuthorize');
 
   // Login Callback (Emulated)
-  Route::get('emulated/idp', 'ThePaulus\Shibboleth\Controllers\ShibbolethController@emulateIdp');
+  Route::get('emulated/idp', ShibbolethController::class . '@emulateIdp');
   // Login Callback (Emulated)
-  Route::post('emulated/idp', 'ThePaulus\Shibboleth\Controllers\ShibbolethController@emulateIdp');
+  Route::post('emulated/idp', ShibbolethController::class . '@emulateIdp');
   // Login Route (Emulated)
-  Route::get('emulated/login', 'ThePaulus\Shibboleth\Controllers\ShibbolethController@emulateLogin');
+  Route::get('emulated/login', ShibbolethController::class . '@emulateLogin');
   // Logout Route (Emulated)
-  Route::get('emulated/logout', 'ThePaulus\Shibboleth\Controllers\ShibbolethController@emulateLogout');
+  Route::get('emulated/logout', ShibbolethController::class . '@emulateLogout');
 });
